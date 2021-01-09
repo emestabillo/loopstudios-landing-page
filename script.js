009851b38282fa4c001aff34c6dc549f6307d9d1
Emmilie Estabillo <emsestabillo@gmail.com>
2021-01-09 07:26:41 -0500

    Add noscroll nav and styles for projects section

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const navToggle = document.querySelector(".nav__toggle");
 const navWrapper = document.querySelector(".nav__wrapper");
 const navIcon = document.querySelector(".menu-icon");
+const body = document.body;
 
 navToggle.addEventListener("click", function () {
   if (navWrapper.classList.contains("active")) {
@@ -8,11 +9,13 @@ navToggle.addEventListener("click", function () {
     this.setAttribute("aria-label", "menu");
     navWrapper.classList.remove("active");
     navIcon.src = "images/icon-hamburger.svg"
+    body.style.overflow = "auto"
     
   } else {
     navWrapper.classList.add("active");
     navIcon.src = "images/icon-close.svg"
     this.setAttribute("aria-label", "close menu");
     this.setAttribute("aria-expanded", "true");
+    body.style.overflow ="hidden";
   }
 });
